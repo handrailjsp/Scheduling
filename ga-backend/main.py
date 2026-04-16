8e7f8e065ade1c997a2fb3ffca11f6f83ae22c70
joachim <joachimmsp@gmail.com>
2026-04-16 15:47:40 +0800

    add frontend url

--- ga-backend/main.py
+++ ga-backend/main.py
@@ -11,7 +11,10 @@ app = FastAPI(title="EQ-Schedule API", version="3.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://scheduling-s69x.vercel.app", 
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
